712ff0c5f0799626114c523313cd32528ceaf90b
Uwe Kubosch <uwe@kubosch.no>
2016-11-28 11:15:38 +0100

    Fixed bug when installing Android SDK tools Reinstall Ruboto gem when testing to get new /lib/* changes Skip emulator snapshot on first install when on travis with new Android version

--- lib/ruboto/util/emulator.rb
+++ lib/ruboto/util/emulator.rb
@@ -227,7 +227,8 @@ EOF
         # FIXME(uwe): or the x86(_64) emulators don't require HAXM anymore
         # Newer Android SDK tools (V24) require HAXM to run x86 emulators.
         # HAXM is not available on travis-ci.
-        if ON_TRAVIS && sdk_level.to_i >= 22
+        avoid_x86 = ON_TRAVIS && sdk_level.to_i >= 22
+        if avoid_x86
           abis.reject!{|a| a =~ /x86/}
           has_x86_64 = nil
           has_x86 = nil
@@ -259,7 +260,7 @@ EOF
         #   File.write(skin_filename, new_skin_config) if new_skin_config != old_skin_config
         # end
 
-        puts `echo no | android create avd -a -n #{avd_name} -t android-#{sdk_level} #{abi_opt} -c 64M #{"-s #{skin}" if skin} -d "#{android_device}"`
+        puts `echo no | android create avd #{'-a' unless avoid_x86} -n #{avd_name} -t android-#{sdk_level} #{abi_opt} -c 64M #{"-s #{skin}" if skin} -d "#{android_device}"`
 
         if $? != 0
           puts 'Failed to create AVD.'
